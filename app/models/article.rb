eafa37b7979a4a78c8edcd823a640385b2736515
AshIgnYeo <contact@ashleyignatius.com>
2022-06-08 02:36:54 +0800

    added bundle lock

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
   has_many_attached :images
+  validates :title, presence: true
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -4,4 +4,16 @@ class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'valid Article' do
+    article = Article.new(title: 'Has a title')
+    assert article.valid?
+  end
+  
+  test 'invalid without title' do
+    article = Article.new
+    assert !article.valid?
+  end
+
+  # test 'invalid without email' do
+  # end
 end
